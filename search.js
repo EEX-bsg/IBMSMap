74eca1ed650832adec7a1c9773c7880b5b5997a4
EEX-bsg <exendra314@gmail.com>
2021-12-06 03:03:06 +0900

    bug fix

--- search.js
+++ search.js
@@ -1,20 +1,20 @@
 "use strict";
 const createSearchResultDOM = function(name, coord, info) {
     const serchResult = document.createElement("div");
-    serchResult.setAttribute("class", "serch-result flex-column");
+    serchResult.setAttribute("class", "search-result flex-column");
     serchResult.setAttribute("name", name);
 
     const pointName = document.createElement("div");
     pointName.setAttribute("class", "point-name");
-    pointName.value = name;
+    pointName.textContent = name;
 
     const pointCoord = document.createElement("div");
     pointCoord.setAttribute("class", "point-coord");
-    pointCoord.value = `x: ${coord.x}, z: ${coord.z}`;
+    pointCoord.textContent = `x: ${coord.x}, z: ${coord.z}`;
 
     const pointInfo = document.createElement("div");
     pointInfo.setAttribute("class", "point-info");
-    pointInfo.value = info;
+    pointInfo.textContent = info;
 
     serchResult.appendChild(pointName);
     serchResult.appendChild(pointCoord);
