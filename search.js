290b209ab9b185c0ff133a1659367c8c02d7f5a0
EEX-bsg <exendra314@gmail.com>
2021-12-06 02:51:39 +0900

    add createSerchResultDOM function

--- search.js
+++ search.js
@@ -1,1 +1,24 @@
 "use strict";
+const createSearchResultDOM = function(name, coord, info) {
+    const serchResult = document.createElement("div");
+    serchResult.setAttribute("class", "serch-result flex-column");
+    serchResult.setAttribute("name", name);
+
+    const pointName = document.createElement("div");
+    pointName.setAttribute("class", "point-name");
+    pointName.value = name;
+
+    const pointCoord = document.createElement("div");
+    pointCoord.setAttribute("class", "point-coord");
+    pointCoord.value = `x: ${coord.x}, z: ${coord.z}`;
+
+    const pointInfo = document.createElement("div");
+    pointInfo.setAttribute("class", "point-info");
+    pointInfo.value = info;
+
+    serchResult.appendChild(pointName);
+    serchResult.appendChild(pointCoord);
+    serchResult.appendChild(pointInfo);
+
+    return serchResult;
+}
